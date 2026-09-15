3386d46acf3a7bd38a3ab74fd1445721d5a4dd04
accessiblepixel <media@accessiblepixel.com>
2026-09-15 12:48:01 +0100

    Bump version number to test update checker.

--- pojo-accessibility.php
+++ pojo-accessibility.php
@@ -4,7 +4,7 @@
  * Plugin Name:       One Click Accessibility 2 Fork
  * Plugin URI:        https://oneclickaccessibility.com
  * Description:       The 'One Click Accessibility' fork without the online account and GDPR implications of the Elementor version. Designed help you make your WordPress website more accessible.
- * Version:           2.2.9
+ * Version:           2.2.9.1
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Tested up to:      7.1
